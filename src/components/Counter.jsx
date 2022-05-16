3c9b669a20166176812668b5c20c8e3b1a02ea2c
Sasy07 <sasan.ghafooryfard@gmail.com>
2022-05-17 01:55:59 +0430

    props default value

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,29 +1,37 @@
+import {Component} from "react";
 
-// class Counter extends Component {
-//
-//     //Stateless
-//     //Statefull
-//     render() {
-//         console.log(this.props);
-//         const {count} = this.props;
-//
-//         return (
-//             <div>
-//                 <p>شمارنده : {count}</p>
-//             </div>
-//         )
-//     }
-// }
+class Counter extends Component {
 
-const Counter = (props) => {
-    console.log(props);
+    //Stateless
+    //Statefull
 
-    return (
-        <div>
-            <p>شمارنده : {props.count}</p>
-            <h3>{props.children}</h3>
-        </div>
-    )
+    static defaultProps ={
+        count : 140
+    }
+    render() {
+        console.log(this.props);
+        const {count} = this.props;
+
+        return (
+            <div>
+                <p>شمارنده : {count }</p>
+            </div>
+        )
+    }
 }
 
+// const Counter = (props) => {
+//     console.log(props);
+//
+//     return (
+//         <div>
+//             <p>شمارنده : {props.count || 90}</p>
+//             <h3>{props.children}</h3>
+//         </div>
+//     )
+// }
+
+// Counter.defaultProps={
+//     count : 25
+// }
 export default Counter;
